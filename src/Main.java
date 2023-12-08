5f48dc181fecc9a0657288c090e259f1bff75044
Olga Pechenkina <Olha.pechyonkina@yandex.ru>
2023-12-08 20:48:38 +0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int price = 13676;
+        int miles = price/20;
+        System.out.println( "Бонус:" + miles);
+    }
+}
